f6665a19a7c37b7a13ac825d27d31c0961273bde
Jamie Houseman <jamie@etg-software.co.uk>
2019-05-11 18:23:11 +0100

    Add api project.

---
+++ Music-Releases-Core/Music-Releases-Core.API/Controllers/ReleaseDetailController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Music_Releases_Core.BL;
+using Music_Releases_Core.Repository.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicReleasesCore.API.Controllers
+{
+    public class ReleaseDetailController : Controller
+    {
+        IAmazonItemRepository _amazonItemRepo;
+        IItunesItemRepository _itunesItemRepo;
+
+        public ReleaseDetailController(IAmazonItemRepository amazonItemRepo, IItunesItemRepository itunesItemRepo)
+        {
+            _amazonItemRepo = amazonItemRepo;
+            _itunesItemRepo = itunesItemRepo;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [Route("api/releasedetail/{asin}")]
+        public IActionResult Get(string asin)
+        {
+            var releaseSearch = new ReleaseSearch(_amazonItemRepo, _itunesItemRepo);
+            MusicReleaseCollection musicReleaseDetailModel = null;
+
+            try
+            {
+                musicReleaseDetailModel = releaseSearch.GetDetails(asin);
+            }
+            catch (WebException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            if (musicReleaseDetailModel != null)
+                return Ok(musicReleaseDetailModel);
+            else
+                return StatusCode(500);
+        }
+    }
+}
--- Music-Releases-Core/Music-Releases-Core.API/Startup.cs
+++ Music-Releases-Core/Music-Releases-Core.API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Music_Releases_Core.Repository;
+using Music_Releases_Core.Repository.Interfaces;
 
 namespace Music_Releases_Core.API
 {
@@ -18,6 +21,9 @@ namespace Music_Releases_Core.API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("passes.json");
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +31,17 @@ namespace Music_Releases_Core.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var AmazonAccessId = Configuration["AmazonAccessId"];
+            var AmazonEndPoint = Configuration["AmazonEndPoint"];
+            var AmazonAssociateTag = Configuration["AmazonAssociateTag"];
+            var AmazonSecretKey = Configuration["AmazonSecretKey"];
+            var ItunesAffiliateId = Configuration["ItunesAffiliateId"];
+            var ItunesRequestUrl = Configuration["ItunesRequestUrl"];
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<IAmazonItemRepository>(s => new AmazonItemRepository(AmazonAccessId, AmazonEndPoint, AmazonAssociateTag, AmazonSecretKey));
+            services.AddTransient<IItunesItemRepository>(s => new ItunesItemRepository(ItunesAffiliateId, ItunesRequestUrl));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
